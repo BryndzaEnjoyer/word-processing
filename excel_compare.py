d21f9bac9632aecc045afa487c0cbff395378b7c
Oliver Ihring <79016913+oliverihring@users.noreply.github.com>
2026-01-16 09:41:50 +0100

    Create excel_compare.py

---
+++ excel_compare.py
@@ -0,0 +1,23 @@
+from openpyxl import Workbook as wb
+from openpyxl import load_workbook
+from pathlib import Path
+
+def get_data_file_path(filename):
+	return Path(__file__).parent / "data/raw" / filename
+
+def get_output_file_path(filename):
+	return Path(__file__).parent / "data/processed" / filename
+
+def compare_results(filename_data, filename_output):
+    doc_data = load_workbook(get_data_file_path(filename_data), data_only=False)
+    doc_output = load_workbook(get_output_file_path(filename_output), data_only=False)
+    sheets_a, sheets_b = doc_data.sheetnames, doc_output.sheetnames
+    common = [s for s in sheets_a if s in sheets_b]
+    conflicts = {}
+    for s in common:
+        sheet_data = doc_data[s]
+        sheet_output = doc_output[s]
+        for row in sheet_data.iter_rows():
+            for cell in row:
+                if cell.value != sheet_output[cell.coordinate].value:
+                    conflicts[cell.coordinate] = (cell.value, sheet_output[cell.coordinate].value)
