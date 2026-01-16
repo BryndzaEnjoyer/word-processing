bccf182917ed67d60dbf640152319c08cf43fe44
Sclex69 <scaredatom@gmail.com>
2026-01-16 09:51:44 +0100

    printing change
    now instead of printing it puts the output in a nested list

--- excel.py
+++ excel.py
@@ -5,23 +5,24 @@ path=get_data_file_path('Výpočty a filtrovanie.xlsx')
 wb = xl.load_workbook(path)
 print(wb.sheetnames)
 
-def select_sheet(x):
 
+def select_sheet(x):
     wb._active_sheet_index = x
     print("Active sheet: ", wb.active)
     sheet_obj = wb.active
     row = sheet_obj.max_row
     column = sheet_obj.max_column
 
-    print("\nValue of first column")
-    for i in range(1, row + 1):
-        cell_obj = sheet_obj.cell(row=i, column=1)
-        print(cell_obj.value)
 
-    print("\nValue of first row")
-    for i in range(1, column + 1):
-        cell_obj = sheet_obj.cell(row=2, column=i)
-        print(cell_obj.value, end=" ")
+    all_values = []
+    for i in range(1, row + 1):
+        row_values = []
+        for j in range(1, column + 1):
+            cell_obj = sheet_obj.cell(row=i, column=j)
+            row_values.append(cell_obj.value)
+        all_values.append(row_values)
+
+    return all_values
 
 y=int(input("select a sheet number:"))
-select_sheet(y)
+print(select_sheet(y))
